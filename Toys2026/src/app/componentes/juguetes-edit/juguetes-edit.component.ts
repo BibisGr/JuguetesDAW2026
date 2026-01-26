6a2372a80b027b63aa14a54d7a55cb565b030a52
Bibis Gr <bertha.garrastacho@digitechfp.com>
2026-01-26 10:39:17 +0100

    base trabajo proyecto Toys2026

---
+++ Toys2026/src/app/componentes/juguetes-edit/juguetes-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-juguetes-edit',
+  imports: [],
+  templateUrl: './juguetes-edit.component.html',
+  styleUrl: './juguetes-edit.component.css'
+})
+export class JuguetesEditComponent {
+
+}
---
+++ Toys2026/src/app/componentes/juguetes-list/juguetes-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-juguetes-list',
+  imports: [],
+  templateUrl: './juguetes-list.component.html',
+  styleUrl: './juguetes-list.component.css'
+})
+export class JuguetesListComponent {
+
+}
