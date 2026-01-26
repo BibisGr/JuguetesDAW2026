b11abfd2b0d513fa4fcf608181342b598db547df
Bibis Gr <bertha.garrastacho@digitechfp.com>
2026-01-26 10:52:42 +0100

    feat2 base estructura, routes, navbar, footer  Toys2026

--- Toys2026/src/app/app.routes.ts
+++ Toys2026/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import {JuguetesListComponent} from './componentes/juguetes-list/juguetes-list.component';
+import {JuguetesEditComponent} from './componentes/juguetes-edit/juguetes-edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'juguete/list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'juguete/list',
+    component: JuguetesListComponent
+  },
+  {
+    path: 'juguete/add',
+    component: JuguetesEditComponent
+  },
+  {
+    path: 'juguete/edit/:id',
+    component: JuguetesEditComponent
+  }
+];
